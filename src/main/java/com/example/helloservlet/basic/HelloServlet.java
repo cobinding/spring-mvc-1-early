14b218541c830c33aa064bb77307011fb704d775
cobinding <cntpqls0218@gmail.com>
2023-11-12 01:37:10 +0900

    mvc

--- src/main/java/com/example/helloservlet/basic/HelloServlet.java
+++ src/main/java/com/example/helloservlet/basic/HelloServlet.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
-    // Servlet 호출 시 생성될 service method
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //super.service(req, resp);
@@ -18,7 +17,7 @@ public class HelloServlet extends HttpServlet {
         System.out.println("request = " + request);
         System.out.println("response = " + response);
 
-        // url에 쿼리 파라미터 넣으면 터미널에 찍히는 코드
+        // url에 쿼리 파라미터 넣으  면 터미널에 찍히는 코드
         String username = request.getParameter("username");
         System.out.println("username = " + username);
 
