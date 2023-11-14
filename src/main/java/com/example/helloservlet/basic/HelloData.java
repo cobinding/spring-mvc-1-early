c9f082fc61845a8c577a6fa78f2c911b3776d280
cobinding <cntpqls0218@gmail.com>
2023-11-14 22:35:31 +0900

    HeaderServletResponse 기본 사용법. Header 편의 메서드

---
+++ src/main/java/com/example/helloservlet/basic/HelloData.java
@@ -0,0 +1,11 @@
+package com.example.helloservlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+    public String username;
+    public int age;
+
+}
